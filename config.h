a65f09698137317fb8f91ecbf82bac25a81d98be
Yaroslav Luzin <jardev@gmail.com>
2018-12-05 18:38:32 +0200

    Changed opacity to optimal

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ char *termname = "xterm-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xfc;
+unsigned int alpha = 0xf0;
 
 static const char *colorname[] = {
 	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
